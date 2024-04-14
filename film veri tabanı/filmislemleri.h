02027cc7b574e598f76729a20ab4dc13a76d4906
Furkan ERTURK <139815208+furkanertrk@users.noreply.github.com>
2024-04-14 15:55:56 +0300

    filmislemleri.h

--- film veri tabanı/filmislemleri.h
+++ film veri tabanı/filmislemleri.h
@@ -1,21 +1,19 @@
 #pragma once
 #include <iostream>
 #include <string>
-#include <vector>
 using namespace std;
-int filmsayar = 0;
 enum tur{dram,komedi,aksiyon,gerilim};
 
 string mevcutdizi[4][4][4] = {
         {"Godfather", "1972", "Dram","Francis Ford Coppola"},
-        {"Taxi Driver","1976", "Suç","Martin Scorsese"},
+        {"Taxi Driver","1976", "SuÃ§","Martin Scorsese"},
         {"Fight Club","1999","Drama","David Fincher"},
         {"Matrix  ", "1999", "Kurgu","Lana/Lilly Wachowski"}
 }
 
 
 ; void mevcutfilmyazdir() {
-    cout << "Ad: " << '\t' << "Vizyon yýlý: " << '\t' << "Tür:" << '\t' << "Yönetmen:" << endl;
+    cout << "Ad: " << '\t' << "Vizyon yÄ±lÄ±: " << '\t' << "TÃ¼r:" << '\t' << "YÃ¶netmen:" << endl;
     int i, j, p;
     for (i = 0; i < 4; i++) {
         for (j = 0; j < 4; j++) {
@@ -31,9 +29,9 @@ string mevcutdizi[4][4][4] = {
 
 
 void anamenu() {
-    cout << "(1) Mevcut filmleri göster" << endl;
+    cout << "(1) Mevcut filmleri gÃ¶ster" << endl;
     cout << "(2) Yeni film ekle" << endl;
-    cout << "(3) Film çýkar" << endl;
-    cout << "(4) Tüm filmleri göster" << endl;
-    cout << "(5) Çýkýþ" << endl;
+    cout << "(3) Film Ã§Ã½kar" << endl;
+    cout << "(4) TÃ¼m filmleri gÃ¶ster" << endl;
+    cout << "(5) Ã‡Ä±kÄ±ÅŸ" << endl;
 }
