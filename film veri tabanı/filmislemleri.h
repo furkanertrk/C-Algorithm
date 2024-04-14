a77dd2e3cfbd071857a834bf6f483c3f4723ca31
Furkan ERTURK <139815208+furkanertrk@users.noreply.github.com>
2024-04-14 15:53:44 +0300

    Add files via upload
    açılan konsoldan film ekleyip çıkartabileceğiniz ufak çaplı bir film veritabanı.

---
+++ film veri tabanı/filmislemleri.h
@@ -0,0 +1,39 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+int filmsayar = 0;
+enum tur{dram,komedi,aksiyon,gerilim};
+
+string mevcutdizi[4][4][4] = {
+        {"Godfather", "1972", "Dram","Francis Ford Coppola"},
+        {"Taxi Driver","1976", "Suç","Martin Scorsese"},
+        {"Fight Club","1999","Drama","David Fincher"},
+        {"Matrix  ", "1999", "Kurgu","Lana/Lilly Wachowski"}
+}
+
+
+; void mevcutfilmyazdir() {
+    cout << "Ad: " << '\t' << "Vizyon yýlý: " << '\t' << "Tür:" << '\t' << "Yönetmen:" << endl;
+    int i, j, p;
+    for (i = 0; i < 4; i++) {
+        for (j = 0; j < 4; j++) {
+            for (p = 0; p < 4; p++)
+            {
+                cout << mevcutdizi[i][j][p]<<'\t';
+            }
+        }
+        cout << endl;
+    }
+
+}
+
+
+void anamenu() {
+    cout << "(1) Mevcut filmleri göster" << endl;
+    cout << "(2) Yeni film ekle" << endl;
+    cout << "(3) Film çýkar" << endl;
+    cout << "(4) Tüm filmleri göster" << endl;
+    cout << "(5) Çýkýþ" << endl;
+}
---
+++ film veri tabanı/structures.cpp
@@ -0,0 +1,73 @@
+﻿#include "filmislemleri.h"
+struct filmsablonu {
+    string film;
+    string yil;
+    string yonetmen;
+    string tur;
+};
+
+int main() {
+    setlocale(LC_ALL, "Turkish");
+    struct filmsablonu yenifilmler[100];
+    int a = 10, i = 0, sil;
+    cout << "*********FİLM VERİ TABANI*********" << endl;
+    
+    anamenu();
+    
+    while (a != 5) {
+        cout << "==>";
+        cin >> a;
+        switch (a) {
+        case 1:
+            mevcutfilmyazdir();
+            break;
+        case 2:
+            for (; i < 100; i++) {
+                cout << "Film adını giriniz:";
+                cin >> yenifilmler[i].film;
+                cout << "Vizyona çıkış tarihi:";
+                cin >> yenifilmler[i].yil;
+                cout << "Tür:";
+                cin >> yenifilmler[i].tur;
+                cout << "Yönetmen:";
+                cin >> yenifilmler[i].yonetmen;
+                cout << "Film eklendi" << endl;
+
+                cout << "Yeni film eklensin mi: (e/h) ";
+                char soru;
+                cin >> soru;
+                if (soru == 'e') continue;
+                else break;
+            }
+            break;
+
+        case 3:
+            cout << "Hangi film silinsin:";
+            cin >> sil;
+            sil -= 2;
+            if (sil >= 0 && sil < i) {
+                for (int h = sil; h < i-1 ; ++h) {
+                    yenifilmler[h] = yenifilmler[h + 1];
+                }
+                i--;
+                cout << "Film silindi" << endl;
+            }
+            else {
+                cout << "Gecersiz indeks!" << endl;
+            }
+        case 4:
+            cout << "Veri tabanındaki mevcut filmler:" << endl;
+            mevcutfilmyazdir();
+            cout << endl;
+            cout << "Eklenen filmler:" << endl;
+            cout << "Ad: " << '\t' << "Vizyon yılı: " << '\t' << "Tür:" << '\t' << "Yönetmen:" << endl;
+            for (int p=0; p <= i; p++) {
+                cout<< yenifilmler[p].film << '\t' << yenifilmler[p].yil <<'\t'<<  yenifilmler[p].tur <<'\t' << yenifilmler[p].yonetmen << endl;
+            }
+            break;
+        case 5:
+            break;
+        }
+     }
+    return 0;
+}
